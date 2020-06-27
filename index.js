d1d2432778f2b4ecdc1104f489a4c788051eaa25
ivankahnybediuk <56602081+ivankahnybediuk@users.noreply.github.com>
2020-06-27 16:45:34 +0300

    3

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.reviews__slider').slick({
+      autoplay: true,
+      speed:500,
+      prevArrow:$("#prev-arrow"),
+      nextArrow:$("#next-arrow")
+    });
+  });
+      
